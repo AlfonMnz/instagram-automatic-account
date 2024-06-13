68ffdb2138cf0879b4783a84f17122e993790ff0
AlfonMnz <alfonmusan14@gmail.com>
2024-06-13 23:46:54 +0200

    finish

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'dotenv/config'
 const ig = new IgApiClient()
 import {readFile} from 'fs';
 import {promisify} from 'util';
-
+import moment from 'moment-timezone';
 const readFileAsync = promisify(readFile);
 
 ig.state.generateDevice(process.env.IG_USERNAME);
@@ -12,12 +12,21 @@ ig.state.generateDevice(process.env.IG_USERNAME);
         const loggedInUser = await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
         const video = await readFileAsync(process.env.VIDEO_PATH);
         const cover = await readFileAsync(process.env.VIDEO_COVER_PATH);
-        const publishResult = await ig.publish.video({
-            video,
-            coverImage: cover,
-            caption: 'Somos rumanos peligrosos #anhqv #mariano #rumanos #aquinohayquienviva'
-        });
-        console.log({publishResult});
+        const momentStartDate = moment.tz(process.env.START_DATE, "Europe/Madrid")
+        setInterval(async () => {
+            let dateInSpain = moment().tz("Europe/Madrid");
+            if (dateInSpain.hours() === 0 && dateInSpain.minutes() === 10) {
+                const diffDays = dateInSpain.diff(momentStartDate, 'days');
+                const publishResult = await ig.publish.video({
+                    video,
+                    coverImage: cover,
+                    caption: `Día: ${diffDays}\n Somos rumanos peligrosos #anhqv #mariano #rumanos #aquinohayquienviva`
+                });
+                console.log({publishResult, dateInSpain});    
+            }
+            
+        }, 60000);
+        
     } catch (e) {
         console.log(e)
     }
